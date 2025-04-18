2723cfe7010d2ed291bd9f540abef9dd627cec90
hfzhang31 <hfzhang31@outlook.com>
2025-04-18 18:27:26 -0400

    Initial commit

---
+++ training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+BASELINE_ACTOR_MODEL_PATH=facebook/opt-1.3b
+ACTOR_MODEL_PATH=~/workspace/siyuan/rlhf/training/step1_supervised_finetuning/output
+CRITIC_MODEL_PATH=~/workspace/siyuan/rlhf/training/step2_reward_model_finetuning/output
+ACTOR_ZERO_STAGE=$3
+CRITIC_ZERO_STAGE=$4
+OUTPUT=$5
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output
+fi
+if [ "$ACTOR_ZERO_STAGE" == "" ]; then
+    ACTOR_ZERO_STAGE=0
+fi
+if [ "$CRITIC_ZERO_STAGE" == "" ]; then
+    CRITIC_ZERO_STAGE=0
+fi
+mkdir -p $OUTPUT
+
+deepspeed --num_gpus 1 main.py \
+   --baseline_actor_model_name_or_path $BASELINE_ACTOR_MODEL_PATH --actor_model_name_or_path $ACTOR_MODEL_PATH --critic_model_name_or_path $CRITIC_MODEL_PATH \
+   --actor_zero_stage $ACTOR_ZERO_STAGE --critic_zero_stage $CRITIC_ZERO_STAGE \
+   --num_padding_at_beginning 1 --gradient_accumulation_steps 2 \
+   --deepspeed --actor_lora_dim 128 --actor_gradient_checkpointing --actor_dropout 0.0 \
+   --output_dir $OUTPUT &> $OUTPUT/training.log
