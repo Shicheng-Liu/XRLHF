efed8d843fab029581f08c0087e9fbe150a69682
Shicheng-Liu <liushicheng_11@126.com>
2025-04-20 13:47:16 -0400

    fix

--- dschat/rlhf/ppo_trainer.py
+++ dschat/rlhf/ppo_trainer.py
@@ -46,13 +46,13 @@ class DeepSpeedPPOTrainer():
     def __init__(self, rlhf_engine, args):
         self.rlhf_engine = rlhf_engine
         self.actor_model = self.rlhf_engine.actor
-        self.actor_model = self.actor_model.to(torch.float32)
+        self.actor_model = self.actor_model.to(torch.bfloat16)
         self.critic_model = self.rlhf_engine.critic
-        self.critic_model = self.critic_model.to(torch.float32)
+        self.critic_model = self.critic_model.to(torch.bfloat16)
         self.ref_model = self.rlhf_engine.ref
-        self.ref_model = self.ref_model.to(torch.float32)
+        self.ref_model = self.ref_model.to(torch.bfloat16)
         self.reward_model = self.rlhf_engine.reward
-        self.reward_model = self.reward_model.to(torch.float32)
+        self.reward_model = self.reward_model.to(torch.bfloat16)
         self.tokenizer = self.rlhf_engine.tokenizer
         self.args = args
         self.max_answer_seq_len = args.max_answer_seq_len
