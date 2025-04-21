3f38df898e84591b91224968391e8500a927bd01
Liu <sfl5539@psu.edu>
2025-04-21 15:32:52 -0400

    eval

--- training/step3_rlhf_finetuning/evaluation_scripts/run_test.sh
+++ training/step3_rlhf_finetuning/evaluation_scripts/run_test.sh
@@ -12,7 +12,7 @@ MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
 CUDA_VISIBLE_DEVICES=6 python test.py \
     --model_name_or_path_baseline facebook/opt-1.3b \
     --model_name_or_path_finetune ~/workspace/siyuan/rlhf/training/step1_supervised_finetuning/output/opt-1.3b/full-hh-rlhf \
-    --model_name_or_path_rlhf output/opt-350m/full-hh-rlhf/actor \
+    --model_name_or_path_rlhf output/opt-1.3b/full-hh-rlhf/actor \
     --data_path /gpu02home/hbz5148/workspace/siyuan/rlhf/dataset/Dahoas/full-hh-rlhf/test.json \
     --model_name_or_path_reward $MODEL_NAME 
     
