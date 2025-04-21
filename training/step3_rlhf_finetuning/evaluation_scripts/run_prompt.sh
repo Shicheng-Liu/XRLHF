924487335735a01451530ab49d289c56988b8143
Liu <sfl5539@psu.edu>
2025-04-21 12:20:46 -0400

    eval

--- training/step3_rlhf_finetuning/evaluation_scripts/run_prompt.sh
+++ training/step3_rlhf_finetuning/evaluation_scripts/run_prompt.sh
@@ -6,7 +6,7 @@
 
 # You can provide two models to compare the performance of the baseline and the finetuned model
 
-CUDA_VISIBLE_DEVICES=3 python prompt_eval.py \
+CUDA_VISIBLE_DEVICES=4 python prompt_eval.py \
     --model_name_or_path_baseline facebook/opt-1.3b \
     --model_name_or_path_finetune ~/workspace/siyuan/rlhf/training/step1_supervised_finetuning/output/opt-1.3b/full-hh-rlhf \
     --model_name_or_path_rlhf output/opt-350m/full-hh-rlhf/actor \
