574da84d98f1610a962c12ac14fc3dd8fa6d75ec
Siyuan Xu <786912649@qq.com>
2025-04-21 21:03:58 -0400

    350m

--- training/step3_rlhf_finetuning/evaluation_scripts/opt/run_350m.sh
+++ training/step3_rlhf_finetuning/evaluation_scripts/opt/run_350m.sh
@@ -6,7 +6,7 @@
 MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
 
 CUDA_VISIBLE_DEVICES=6 python test.py \
-    --reward_model_name opt-1.3b \
+    --reward_model_name opt-350m \
     --model_name_or_path_baseline facebook/opt-1.3b \
     --model_name_or_path_finetune ~/workspace/siyuan/rlhf/training/step1_supervised_finetuning/output/opt-1.3b/full-hh-rlhf \
     --model_name_or_path_rlhf output/opt-350m/full-hh-rlhf/actor \
