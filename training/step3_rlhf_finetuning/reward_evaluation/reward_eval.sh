a4468a536cfb1de97f89575d2c845163dd922303
Liu <sfl5539@psu.edu>
2025-04-22 16:34:14 -0400

    reward

--- training/step3_rlhf_finetuning/reward_evaluation/reward_eval.sh
+++ training/step3_rlhf_finetuning/reward_evaluation/reward_eval.sh
@@ -2,7 +2,8 @@
 
 # trained by opt-1.3b reward model
 
-MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
+#MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
+MODEL_NAME=~/workspace/siyuan/rlhf/training/step2_reward_model_finetuning/output/opt-1.3b/full-hh-rlhf
 
 CUDA_VISIBLE_DEVICES=6 python reward_eval.py \
     --data_path opt-1.3b_test_result.json \
